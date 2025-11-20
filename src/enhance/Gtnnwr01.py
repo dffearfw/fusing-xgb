177f5023dcd5f72a2fa0dfc466fe2ae90311ab6f
dffearfw <1658217791@qq.com>
2025-11-20 10:41:49 +0800

    0.1.4

--- src/enhance/Gtnnwr01.py
+++ src/enhance/Gtnnwr01.py
@@ -3,40 +3,59 @@ import sys
 import numpy as np
 import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
-from gnnwr.datasets import init_dataset
+from gnnwr.datasets import init_dataset_split
 from gnnwr.models import GTNNWR
 from visualizer import plot_gtnnwr_results, plot_multiple_models_results
 
 
 data = pd.read_excel('lu_onehot.xlsx')
 data["id"] = np.arange(len(data))
-train_dataset, val_dataset, test_dataset = init_dataset(data=data,
-                                                        test_ratio=0.15,
-                                                        valid_ratio=0.1,
-                                                        x_column=[
-                                                            'aspect', 'slope', 'eastness', 'tpi', 'curvature1',
-                                                            'curvature2', 'elevation', 'std_slope',
-                                                            'std_eastness', 'std_tpi', 'std_curvature1',
-                                                            'std_curvature2', 'std_high', 'std_aspect', 'glsnow',
-                                                            'cswe', 'snow_depth_snow_depth',
-                                                            'ERA5温度_ERA5温度', 'era5_swe', 'gldas',
-                                                            'scp_start', 'scp_end', 'd1', 'd2',
-                                                            'Z', 'da', 'db', 'dc', 'dd',
-                                                            'landuse_11', 'landuse_12', 'landuse_21',
-                                                            'landuse_22', 'landuse_23', 'landuse_24',
-                                                            'landuse_31', 'landuse_32', 'landuse_33',
-                                                            'landuse_41', 'landuse_42', 'landuse_43',
-                                                            'landuse_46', 'landuse_51', 'landuse_52',
-                                                            'landuse_53', 'landuse_62', 'landuse_63',
-                                                            'landuse_64'
-                                                        ],
-                                                        y_column=['swe'],
-                                                        spatial_column=['X', 'Y'],
-                                                        temp_column=['doy','year','month'],
-                                                        id_column=['id'],
-                                                        use_model="gtnnwr",
-                                                        sample_seed=48,
-                                                        batch_size=128)
+# 添加混合分割策略
+data['station_id'] = data['X'].astype(str) + '_' + data['Y'].astype(str)
+
+# 第一阶段：站点分割，划出测试集
+unique_stations = data['station_id'].unique()
+np.random.seed(48)
+np.random.shuffle(unique_stations)
+
+test_stations = unique_stations[:int(len(unique_stations) * 0.1)]
+remaining_stations = unique_stations[int(len(unique_stations) * 0.1):]
+
+test_data = data[data['station_id'].isin(test_stations)].copy()
+remaining_data = data[data['station_id'].isin(remaining_stations)].copy()
+
+# 第二阶段：时间分割，划出训练和验证集
+remaining_data_sorted = remaining_data.sort_values(by=['year', 'month', 'doy'])
+valid_sample_count = int(len(remaining_data_sorted) * 0.15)
+valid_data = remaining_data_sorted.iloc[:valid_sample_count].copy()
+train_data = remaining_data_sorted.iloc[valid_sample_count:].copy()
+
+train_dataset, val_dataset, test_dataset = init_dataset_split(train_data=train_data,
+                                                              val_data=valid_data,
+                                                              test_data=test_data,
+                                                              x_column=[
+                                                                  'aspect', 'slope', 'eastness', 'tpi', 'curvature1',
+                                                                  'curvature2', 'elevation', 'std_slope',
+                                                                  'std_eastness', 'std_tpi', 'std_curvature1',
+                                                                  'std_curvature2', 'std_high', 'std_aspect', 'glsnow',
+                                                                  'cswe', 'snow_depth_snow_depth',
+                                                                  'ERA5温度_ERA5温度', 'era5_swe', 'gldas',
+                                                                  'scp_start', 'scp_end', 'd1', 'd2',
+                                                                  'Z', 'da', 'db', 'dc', 'dd',
+                                                                  'landuse_11', 'landuse_12', 'landuse_21',
+                                                                  'landuse_22', 'landuse_23', 'landuse_24',
+                                                                  'landuse_31', 'landuse_32', 'landuse_33',
+                                                                  'landuse_41', 'landuse_42', 'landuse_43',
+                                                                  'landuse_46', 'landuse_51', 'landuse_52',
+                                                                  'landuse_53', 'landuse_62', 'landuse_63',
+                                                                  'landuse_64'
+                                                              ],
+                                                              y_column=['swe'],
+                                                              spatial_column=['X', 'Y'],
+                                                              temp_column=['doy','year','month'],
+                                                              id_column=['id'],
+                                                              use_model="gtnnwr",
+                                                              batch_size=128)
 
 
 optimizer_params = {
@@ -54,5 +73,4 @@ gtnnwr.run(10,1000)
 gtnnwr.result()
 save_path = "../demo_result/gtnnwr_runs/GTNNWR_DSi_results.png"
 
-# 暂时注释掉可视化，以防 visualizer 模块有问题
 metrics = plot_gtnnwr_results(gtnnwr, save_path=save_path, show_plot=True)
